a217ec0fc288b3e4df29605862ab6743a4560586
Izaan Kaskar <izaankaskar7@gmail.com>
2026-07-10 19:43:26 +0530

    Update main.c

---
+++ universal-os/kernel/main.c
@@ -0,0 +1,10 @@
+#include "../shared/kernel.h"
+
+void kernel_main(void) {
+    hw_init();
+    hw_print("Universal OS Core Loaded Successfully.\n");
+    
+    while(1) {
+        // Main OS execution loop
+    }
+}
